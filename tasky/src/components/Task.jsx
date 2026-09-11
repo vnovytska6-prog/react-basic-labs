98e00f9aaf983d8f51e7729842c8733c4b491585
Viktoriia Novytska <viktoriianovytska@student.kerrycollege.ie>
2026-09-11 16:19:04 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,13 @@
+const Task = (props) => {
+    
+    return (
+        <div className="card">
+            <p>{props.title}</p>
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p className="description">{props.description}</p>
+        </div>
+    )
+}
+
+export default Task;
