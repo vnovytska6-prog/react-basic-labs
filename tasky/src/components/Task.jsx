a7db4bca03c0472849e7806399da9736211c3267
Viktoriia Novytska <viktoriianovytska@student.kerrycollege.ie>
2026-09-18 15:01:40 +0100

    Added keys to tasks

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -4,6 +4,7 @@ const Task = (props) => {
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
+            <p> {props.children}</p>
             <p className="description">{props.description}</p>
         </div>
     )
